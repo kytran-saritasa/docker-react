2862a466f4b3dc0bc5633e5790b8a451458fee50
kytran-saritasa <ky.tran@saritasa.com>
2024-10-16 13:21:40 +0700

    Praise GOD

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Praise GOD.
           Hi there! Go learn react.
         </p>
         <a
